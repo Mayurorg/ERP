71922e77368ab925e0c38cf20d8631198504162c
MayurGaoner <mayurgaoner@gmail.com>
2024-11-15 20:18:07 +0530

    add project

---
+++ Android_CRUD_app/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Android_CRUD_app.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Android_CRUD_app.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Android_CRUD_app/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Android_CRUD_app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
